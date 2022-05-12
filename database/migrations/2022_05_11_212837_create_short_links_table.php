8859a45d090a7609c08a7f98bbef354bf5505040
Kevin Williams <kwilliams9285@gmail.com>
2022-05-11 19:51:23 -0500

    Migration Table - Short Links - Jobs

---
+++ database/migrations/2022_05_11_212837_create_short_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShortLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('short_links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->string('shrunk_url');
+            $table->string('title')->nullable();
+            $table->integer('tries')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('short_links');
+    }
+}
